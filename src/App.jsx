e9ff4ccc4f11d7b84a2429d3ca0b2bf01e1a5cfc
unknown <m0njer0.002@gmail.com>
2024-03-07 18:34:43 +0400

    header-scheme

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import { useState } from 'react'
+import './App.css'
+// import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Header from './conponents/header/Header'
+
+
+
+
+function App() {
+  return (
+<>
+<Header />
+
+</>
+  )
+}
+
+export default App
---
+++ src/conponents/header/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import classNames from 'classnames';
+import styles from './header.module.css';
+
+
+const Header = () => {
+  return (
+    <div className={classNames(styles.header)}>
+        <div className={classNames(styles.header_up)}>
+          <div className={classNames(styles.header_up_contacts)}>222222</div>
+          <div className={classNames(styles.header_up_search)}>33333333</div>
+          <div className={classNames(styles.header_up_soscials)}>4444444444</div>
+        </div>
+        <div className={classNames(styles.header_down)}>
+          <div className={classNames(styles.header_down_logo)}></div>
+          <ul className={classNames(styles.header_down_nav)}>
+            <li><a href="">Home</a></li>
+            <li><a href="">Shop</a></li>
+            <li><a href="">Grooming</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Pages</a></li>
+          </ul>
+          <div className={classNames(styles.header_down_selling)}></div>
+        </div>
+      </div>
+  )
+}
+
+export default Header
